902e4c0b5129cbc5640544e63af4782d2ff7081a
Marcelo-Dev-Android <developermarcelosouza@gmail.com>
2021-10-29 17:07:09 -0300

    Header and Toolbar done

--- app/src/main/java/com/marcelo/itipix/ConfirmationActivity.kt
+++ app/src/main/java/com/marcelo/itipix/ConfirmationActivity.kt
@@ -3,9 +3,9 @@ package com.marcelo.itipix
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ConfirmationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_confirmation)
     }
 }
