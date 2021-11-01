333a3635e15bf21c7dc6a209fc13e5930123d3d6
Marcelo-Dev-Android <developermarcelosouza@gmail.com>
2021-11-01 00:24:21 -0300

    Header done, Body cards done

--- app/src/main/java/com/marcelo/itipix/ConfirmationActivity.kt
+++ app/src/main/java/com/marcelo/itipix/ConfirmationActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class ConfirmationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_confirmation)
+        setContentView(R.layout.activity_pix)
     }
 }
