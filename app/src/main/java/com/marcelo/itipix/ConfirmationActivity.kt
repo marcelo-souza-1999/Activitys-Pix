89d68f177a9b8b8ef345f2b506d4ad18e4ca319d
Marcelo-Dev-Android <developermarcelosouza@gmail.com>
2021-11-01 11:18:40 -0300

    Components UI Pix finished

--- app/src/main/java/com/marcelo/itipix/ConfirmationActivity.kt
+++ app/src/main/java/com/marcelo/itipix/ConfirmationActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class ConfirmationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pix)
+        setContentView(R.layout.activity_confirmation)
     }
 }
